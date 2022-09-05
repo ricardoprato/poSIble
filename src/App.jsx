f743e4eb62aa496d6c62fa4bda1b994cd2f80653
Miguel-Huaman <miguelangelhh17@gmail.com>
2022-09-05 18:54:19 -0500

    new: metamask connection

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,71 @@ import { Faq } from "./pages/Faq";
 
 import { Form } from "./pages/Form";
 
+import { useState } from "react"; // storing data in the state
+import { ethers } from "ethers"; // interacting with wallet
+
 function App() {
+
+  const [publicKey, setPublickey] = useState();
+  const [network, setNetwork] = useState();
+  const [chainId, setChainId] = useState();
+  const [msg, setMsg] = useState();
+
+  const connectButton = async () => {
+    const { ethereum } = window;
+    if (ethereum.isMetaMask) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const accounts = await provider.send("eth_requestAccounts", []);
+
+      const { name, chainId } = await provider.getNetwork();
+
+      setNetwork(name);
+      setChainId(chainId);
+      setPublickey(accounts[0]);
+    } else {
+      setMsg("Install MetaMask");
+    }
+  };
+
+  const swithcNetwork = async (chainId) => {
+    try {
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [avlNetwork[`${chainId}`]]
+      });
+      setNetwork(avlNetwork[`${chainId}`].chainName);
+      setChainId(chainId);
+    } catch (error) {
+      setMsg(error);
+    }
+  };
+
+  
+const avlNetwork = {
+  137: {
+    chainId: `0x${Number(137).toString(16)}`,
+    rpcUrls: ["https://rpc-mainnet.matic.network/"],
+    chainName: "Polygon Mainnet",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18
+    },
+    blockExplorerUrls: ["https://polygonscan.com/"]
+  },
+  43114: {
+    chainId: `0x${Number(43114).toString(16)}`,
+    rpcUrls: ["https://api.avax.network/ext/bc/C/rpc"],
+    chainName: "Avalanche C-Chain",
+    nativeCurrency: {
+      name: "Avalanche",
+      symbol: "AVAX",
+      decimals: 18
+    },
+    blockExplorerUrls: ["https://snowtrace.io/"]
+  }
+};
+
   return (
     <>
       <BrowserRouter>
@@ -21,6 +85,20 @@ function App() {
           <Route exact path="/faq" element={<Faq />} />
         </Routes>
         <Footer />
+        <h1>Connect MetaMask</h1>
+      <button onClick={connectButton}>Connect Wallet</button>
+      <br />
+      <button className="btn" onClick={() => swithcNetwork(137)}>
+        Connect Polygon
+      </button>
+      <br />
+      <button className="btn" onClick={() => swithcNetwork(43114)}>
+        Connect Avalanche
+      </button>
+      <p>Public Key: {publicKey}</p>
+      <p>Network: {network}</p>
+      <p>Chain ID : {chainId}</p>
+      {msg && <p>{msg}</p>}
       </BrowserRouter>
     </>
   );
