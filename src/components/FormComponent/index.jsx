30347814afba508e58b3fc4feb94ef903aa475bc
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2022-09-05 00:11:41 -0500

    Estilos del form actualizados

--- src/components/FormComponent/index.jsx
+++ src/components/FormComponent/index.jsx
@@ -4,25 +4,25 @@ export const FormComponent = () => {
   return (
     <div class="px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
       <div class="max-w-lg mx-auto">
-        <h1 class="text-2xl font-bold text-center text-indigo-600 sm:text-3xl">Empieza tu sueño aqui</h1>
+        <h1 class="text-2xl font-bold text-center text-secondary sm:text-3xl">¡Empieza tu sueño aqui!</h1>
     
-        <p class="max-w-md mx-auto mt-4 text-center text-gray-500">
-          Completa los campos correspondientes para conocer más acerca de tú proyecto
+        <p class="max-w-md mx-auto mt-4 text-center text-white">
+          Completa los campos correspondientes para conocer más acerca de ti 
         </p>
     
-        <div class="p-8 bg-white rounded-lg shadow-lg lg:p-12 lg:col-span-3">
+        <div class="p-8 mt-8 bg-white border-2 border-black rounded-lg shadow-lg lg:p-12 lg:col-span-3">
           
         <form action="" class="space-y-4">
           <div>
             <label class="sr-only" for="name">Name</label>
-            <input class="w-full p-3 text-sm border-gray-200 rounded-lg" placeholder="Nombre del proyecto" type="text" id="name" />
+            <input class="w-full p-3 text-sm border-gray-200 rounded-lg" placeholder="Nombre completo" type="text" id="name" />
           </div>
 
           <div>
             <label class="sr-only" for="message">Descripcion del proyecto</label>
             <textarea
               class="w-full p-3 text-sm border-gray-200 rounded-lg"
-              placeholder="Descripcion"
+              placeholder="Cuentanos sobre ti y cómo planeas utilizar esta oportunidad para transformar tu vida y tu entorno"
               rows="8"
               id="message"
             ></textarea>
