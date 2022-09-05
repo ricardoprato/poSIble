b9f57a9384ef97a863f7404bb7bda4bd96fe9893
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2022-09-05 16:11:11 -0500

    CHANGE: Estilos de la lista de estudiantes actualizado

--- src/components/FormComponent/index.jsx
+++ src/components/FormComponent/index.jsx
@@ -4,7 +4,7 @@ export const FormComponent = () => {
   return (
     <div class="px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
       <div class="max-w-lg mx-auto">
-        <h1 class="text-2xl font-bold text-center text-secondary sm:text-3xl">¡Empieza tu sueño aqui!</h1>
+        <h1 class="text-2xl font-bold text-center text-secondary sm:text-4xl">¡Empieza tu sueño aqui!</h1>
 
         <p class="max-w-md mx-auto mt-4 text-center text-white">
           Completa los campos correspondientes para conocer más acerca de ti
@@ -17,6 +17,10 @@ export const FormComponent = () => {
               <label htmlFor="name">Nombre Completo</label>
               <input class="w-full p-3 text-sm border-gray bg-lightGray rounded-lg" placeholder="Tu Nombre" type="text" id="name" />
             </div>
+            <div>
+              <label htmlFor="name">Ocupación</label>
+              <input class="w-full p-3 text-sm border-gray bg-lightGray rounded-lg" placeholder="A qué te dedicas" type="text" id="occupation" />
+            </div>
 
             <div>
               <label htmlFor="message">Descripción de ti</label>
@@ -24,13 +28,26 @@ export const FormComponent = () => {
                 class="w-full p-3 text-sm border-gray bg-lightGray rounded-lg"
                 placeholder="Cuentanos sobre ti y cómo planeas utilizar esta oportunidad para transformar tu vida y tu entorno"
                 rows="8"
-                id="message"
+                id="description"
               ></textarea>
+            </div>
+            <div>
+              <label htmlFor="platform">Plataforma</label>
+              <nav class="flex flex-col space-y-1">
+                <select name="platform" id="platform">
+                  <option value="Platzi">Platzi (0.10 ETH)</option>
+                  <option value="CoderHouse">CoderHouse (0.06)</option>
+                  <option value="EDteam">EDteam (0.15 ETH)</option>
+                  <option value="Blockdemy">Blockdemy (0.05 ETH)</option>
+                  <option value="Crehana">Crehana (0.05 ETH)</option>
+                  <option value="CodigoFacilito">Código Facilito (0.03 ETH)</option>
+                </select>
+              </nav>
             </div>
             <div>
               <label htmlFor="country">País</label>
               <nav class="flex flex-col space-y-1">
-                <select name="platform" id="platform">
+                <select ocupation="platform" id="platform">
                   <option value="Argentina">Argentina</option>
                   <option value="Bolivia">Bolivia</option>
                   <option value="Chile">Chile</option>
@@ -46,19 +63,6 @@ export const FormComponent = () => {
                 </select>
               </nav>
             </div>
-            <div>
-              <label htmlFor="platform">Plataforma</label>
-              <nav class="flex flex-col space-y-1">
-                <select name="platform" id="platform">
-                  <option value="Platzi">Platzi (0.10 ETH)</option>
-                  <option value="CoderHouse">CoderHouse (0.06)</option>
-                  <option value="EDteam">EDteam (0.15 ETH)</option>
-                  <option value="Blockdemy">Blockdemy (0.05 ETH)</option>
-                  <option value="Crehana">Crehana (0.05 ETH)</option>
-                  <option value="CodigoFacilito">Código Facilito (0.03 ETH)</option>
-                </select>
-              </nav>
-            </div>
 
             {/* <div>
               <label htmlFor="name">Cantidad Minima</label>
@@ -70,7 +74,7 @@ export const FormComponent = () => {
               <input type="date" name="" id="" aria-label='' class='w-full p-3 border-gray-200 rounded-lg' />
             </div> */}
 
-            <div class="mt-4">
+            <div class="pt-4">
               <button
                 type="submit"
                 class="inline-flex items-center justify-center w-full px-5 py-3 text-black bg-secondary border-2 border-black hover:bg-primary rounded-lg sm:w-auto"
